8325c85824786b3cd08c2e0e63cb3144f798fd3f
Somdev Basu <somdevbasu100@gmail.com>
2019-06-25 17:14:11 +0530

    Update Titles.js

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -17,7 +17,7 @@ import React from 'react'
 const Titles = () => {
   return(
     <div>
-      <h1>Weather Finder</h1>
+      <h1>Weathering</h1>
       <p>Find out temperature conditions and more!</p>
     </div>
   );
